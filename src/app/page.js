b8563f65f7e8b8d70c57f66765a40d4a20781f07
sanjeetk-dev <sanjeetk.dev@gmail.com>
2024-10-07 10:47:07 +0530

    changing structure

--- src/app/page.js
+++ src/app/page.js
@@ -24,17 +24,18 @@ export default function Home() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-gray-900 transition duration-300 overflow-hidden">
       <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100 mb-4">Motivational Sloth</h1>
       <QuoteCard quote={quote} />
-      <div className="grid grid-flow-row auto-rows-max mt-4">
-      {themes.map((theme) => (
-        <button
-          key={theme}
-          className="bg-gray-200 dark:bg-gray-600 text-gray-800 dark:text-gray-200 py-2 px-4 rounded transition duration-300 hover:bg-gray-300 dark:hover:bg-gray-500"
-          onClick={() => handleClick(theme)}
-        >
-          {theme}
-        </button>
-      ))}
-    </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-4">
+  {themes.map((theme) => (
+    <button
+      key={theme}
+      className="bg-gray-200 dark:bg-gray-600 text-gray-800 dark:text-gray-200 py-2 px-4 rounded transition duration-300 hover:bg-gray-300 dark:hover:bg-gray-500"
+      onClick={() => handleClick(theme)}
+    >
+      {theme}
+    </button>
+  ))}
+</div>
+
     </div>
   );
 }
