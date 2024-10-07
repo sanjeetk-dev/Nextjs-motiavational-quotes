b1ac0bb89908d73b710dc6dbc2142ee7c1379757
sanjeetk-dev <sanjeetk.dev@gmail.com>
2024-10-07 08:10:31 +0530

    changing structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,42 @@
 "use client"
-
-
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import MotivationalSloth from 'motivational-sloth';
 import QuoteCard from '../components/QuoteCard';
 import ThemeSelector from '../components/ThemeSelector';
 
 export default function Home() {
   const sloth = new MotivationalSloth();
-  const themes = sloth.getThemes();
-  const [selectedTheme, setSelectedTheme] = useState(themes[0]);
-  const quote = sloth.getQuote(selectedTheme);
-
+  const [selectedTheme, setSelectedTheme] = useState("");
+  const [quote, setQuote] = useState("")
+  
+  const handleClick = (e) => {
+    setSelectedTheme(e);
+    setQuote(sloth.getQuote(selectedTheme))
+  }
+  
+  useEffect(()=>{
+    const themes = sloth.getThemes();
+    setSelectedTheme(themes[0])
+    setQuote(sloth.getQuote(selectedTheme))
+  },[])
+  
+  
+  
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-gray-900 transition duration-300 overflow-hidden">
       <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100 mb-4">Motivational Sloth</h1>
       <QuoteCard quote={quote} />
-      <ThemeSelector themes={themes} onSelectTheme={setSelectedTheme} />
+      <div className="grid grid-flow-row auto-rows-max mt-4">
+      {themes.map((theme) => (
+        <button
+          key={theme}
+          className="bg-gray-200 dark:bg-gray-600 text-gray-800 dark:text-gray-200 py-2 px-4 rounded transition duration-300 hover:bg-gray-300 dark:hover:bg-gray-500"
+          onClick={() => handleClick(theme)}
+        >
+          {theme}
+        </button>
+      ))}
+    </div>
     </div>
   );
 }
